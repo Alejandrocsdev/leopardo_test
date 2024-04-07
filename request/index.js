60fc9e567786c0f8076a3542075fd7bd95b772b1
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-04-07 10:11:53 +0800

    request.params.id

--- request/index.js
+++ request/index.js
@@ -1,16 +1,28 @@
 const url = require('url')
-function requestMethods(request, route) {
+const requestMethods = (request, route) => {
+  const path = url.parse(request.url).pathname
   request.params = {}
 
-  const path = url.parse(request.url).pathname
   const pathSegments = path.split('/')
   const routeSegments = route.path.split('/')
+
+  if (routeSegments.length !== pathSegments.length) {
+    return false // Skip this route if the number of segments doesn't match
+  }
+
   for (let i = 0; i < routeSegments.length; i++) {
     if (routeSegments[i].startsWith(':')) {
       const paramName = routeSegments[i].substring(1)
       request.params[paramName] = pathSegments[i]
+      // Skip dynamic segments
+      continue
+    }
+    if (routeSegments[i] !== pathSegments[i]) {
+      return false
     }
   }
+
+  return request.method === route.method
 }
 
 module.exports = requestMethods
--- routes/index.js
+++ routes/index.js
@@ -61,32 +61,7 @@ class Router {
 
     const processRoutes = () => {
       for (const [id, route] of routes) {
-        // requestMethods(request, route)
-        request.params = {}
-
-        const pathSegments = path.split('/')
-        const routeSegments = route.path.split('/')
-
-        if (routeSegments.length !== pathSegments.length) {
-          continue // Skip this route if the number of segments doesn't match
-        }
-
-        let match = true
-        for (let i = 0; i < routeSegments.length; i++) {
-          if (routeSegments[i].startsWith(':')) {
-            const paramName = routeSegments[i].substring(1)
-            request.params[paramName] = pathSegments[i]
-            // Skip dynamic segments
-            continue
-          }
-          if (routeSegments[i] !== pathSegments[i]) {
-            match = false
-            break
-          }
-        }
-
-        if (match && request.method === route.method) {
-          
+        if (requestMethods(request, route)) {
           route.handler(request, response)
           return
         }
